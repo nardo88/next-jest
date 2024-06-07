75580eede4fd8e1dbd78cc86bd7c8d47157c9b3d
nardo88 <nardo1988@mail.ru>
2024-06-07 10:55:22 +0300

    123

--- features/Profile/ui/Main/Main.test.tsx
+++ features/Profile/ui/Main/Main.test.tsx
@@ -1,9 +1,25 @@
+//
+
 import { render } from '@testing-library/react'
 import { Main } from './Main'
+import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime'
+import { createMockrouter } from '../../../../helpers/tests/useRouter'
+
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    query: { id: '123' },
+  }),
+}))
 
 describe('test Profile', () => {
   test('first', () => {
-    render(<Main />)
+    render(
+      <RouterContext.Provider value={createMockrouter({})}>
+        <Main />
+      </RouterContext.Provider>
+    )
     expect(true).toBe(true)
   })
 })
+
+//
